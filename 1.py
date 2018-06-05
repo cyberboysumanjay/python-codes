df5ff2d01a84a8451ea8eacfd6f4469097422fdb
Cyberboy <cyberboysumanjay@gmail.com>
2018-06-05 22:13:22 +0530

    Day 1 Python Code

---
+++ 1.py
@@ -0,0 +1,60 @@
+print ("Hello World")
+#This prints hello world
+#This is a single line comment
+"""
+Multiple
+Line
+Comment
+"""
+#Indentation
+if True:
+	print ("Good")
+print ("Better","Awesome")
+print ("Best")
+#Declaring Variables
+a=3.47
+#Declaring multiple variables at once
+b,c=7,9
+print (a)
+print (b,c)
+#Swapping numbers
+b,c=c,b
+print (b,c)
+#Boolean
+bool1=True
+print (bool1)
+#Datatypes
+int1=2
+f1=2.6
+str1="Jay"
+print (int1,f1,str1)
+#Taking input
+"""
+i=input("Enter anything: ")
+print (i)
+j=int(input("Enter a number: "))
+print (j)
+k=float(input("Enter another number: "))
+print (k)
+"""
+#Arithmetic Operators
+print (b+c)
+print (b-c)
+print (b*c)
+print (b//c)
+print (b/c)
+print (b%c)
+print (b**c)
+#Comparision Operator
+print(b==c)
+print(b!=c)
+print (c>b)
+#Short Hand Notation
+b+=c
+print(b)
+c=c/a
+print (c)
+#Logical Operator
+print(a==b and b==c)
+print(a==b or b==c)
+print(not b)
