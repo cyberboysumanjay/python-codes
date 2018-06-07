f5c2678759907c352ab06e6d1b551d6a079fdbcc
cyberboysumanjay <cyberboysumanjay@gmail.com>
2018-06-07 09:54:48 +0530

    Added another method in function.

--- 3a3.py
+++ 3a3.py
@@ -11,3 +11,16 @@ if (m==0):
     print(1)
 else:
     print (count(m))
+
+
+#Another method
+def count2(n):
+    if n<=9:
+        if n==0:
+            return 1
+        else :
+            return 0
+    if n%10==0:
+        return 1+count2(n//10)
+    return count2(n//10)
+print(count2(m))
